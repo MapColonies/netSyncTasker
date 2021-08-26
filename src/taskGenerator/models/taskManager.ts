b62dbe8a0ad20e8a9bb33847a2b681ab52238b0b
Shahar s <cl_shaharshavit@rnd-hub.com>
2021-08-26 09:18:54 +0300

    chore: cr

--- src/taskGenerator/models/taskManager.ts
+++ src/taskGenerator/models/taskManager.ts
@@ -1,6 +1,6 @@
 import { inject, singleton } from 'tsyringe';
 import { Feature, MultiPolygon, Polygon } from '@turf/turf';
-import { degreesPerPixelToZoomLevel } from '@map-colonies/mc-utils';
+import { degreesPerPixelToZoomLevel, ILogger } from '@map-colonies/mc-utils';
 import { JobManagerClient } from '@map-colonies/mc-priority-queue';
 import { CatalogClient } from '../../clients/catalogClient';
 import { Services } from '../../common/constants';
@@ -14,16 +14,18 @@ export class TaskManager {
     private readonly catalog: CatalogClient,
     private readonly batcher: TileBatcher,
     @inject(Services.CONFIG) config: IConfig,
-    private readonly jobsClient: JobManagerClient
+    private readonly jobsClient: JobManagerClient,
+    @inject(Services.LOGGER) private readonly logger: ILogger
   ) {
     this.batchSize = config.get<number>('batchSize');
   }
 
   public async createBatchedTasks(resourceId: string, resourceVersion: string, jobId: string): Promise<void> {
+    this.logger.info(`create batched tiles tasks for job: ${jobId}, layer: ${resourceId}-${resourceVersion}`);
     const layer = await this.catalog.getDiscreteMetadata(resourceId, resourceVersion);
-    const footPrint = layer.metadata?.footprint as Polygon | Feature<Polygon> | Feature<MultiPolygon>;
-    const zoom = degreesPerPixelToZoomLevel(layer.metadata?.resolution as number);
-    const batchGen = this.batcher.tileBatchGenerator(this.batchSize, footPrint, zoom);
+    const footprint = layer.metadata?.footprint as Polygon | Feature<Polygon> | Feature<MultiPolygon>;
+    const zoomLevel = degreesPerPixelToZoomLevel(layer.metadata?.resolution as number);
+    const batchGen = this.batcher.tileBatchGenerator(this.batchSize, footprint, zoomLevel);
     for (const batch of batchGen) {
       //TODO: check if task exists?
       const parameters = {
