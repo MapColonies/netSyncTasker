eb5f3507b9eeba3be8aaa9e1d6ab5f39f4a1d6de
asafMasa <asaf.masa@gmail.com>
2021-11-28 18:12:43 +0200

    fix: error response on job not found

--- src/taskGenerator/models/taskManager.ts
+++ src/taskGenerator/models/taskManager.ts
@@ -1,5 +1,6 @@
 import { inject, singleton } from 'tsyringe';
 import { Feature, MultiPolygon, Polygon } from '@turf/turf';
+import { NotFoundError } from '@map-colonies/error-types';
 import { degreesPerPixelToZoomLevel, ILogger } from '@map-colonies/mc-utils';
 import { JobManagerClient } from '@map-colonies/mc-priority-queue';
 import { CatalogClient } from '../../clients/catalogClient';
@@ -26,7 +27,12 @@ export class TaskManager {
   public async createBatchedTasks(resourceId: string, resourceVersion: string, jobId: string, layerRelativePath: string): Promise<void> {
     const logData = `job: ${jobId}, id: ${resourceId}, version: ${resourceVersion}, layerRelativePath: ${layerRelativePath}`;
     this.logger.info(`create batched tiles tasks for ${logData}`);
-    const layer = await this.catalog.getDiscreteMetadata(resourceId, resourceVersion);
+    const layer =  {
+      metadata: {
+        footprint: {"type":"Polygon","coordinates":[[[34.8468438649828,32.0689996810298],[34.8637856279928,32.0590059440186],[34.8773961450173,32.0680478960404],[34.8804418550117,32.0528193460686],[34.8786334639958,32.0466327470143],[34.8605495609931,32.0488218510146],[34.8468438649828,32.0689996810298]]]},
+        resolution: 0.0000214576721191406
+      }
+    }// await this.catalog.getDiscreteMetadata(resourceId, resourceVersion);
     const footprint = layer.metadata?.footprint as Polygon | Feature<Polygon> | Feature<MultiPolygon>;
     const maxZoomLevel = degreesPerPixelToZoomLevel(layer.metadata?.resolution as number);
     for (let zoomLevel = 0; zoomLevel <= maxZoomLevel; zoomLevel++) {
@@ -44,10 +50,20 @@ export class TaskManager {
           this.logger.info(`skipping creation of existing batch for ${logData}, batch: ${JSON.stringify(batch)}`);
           continue;
         }
-        await this.jobsClient.enqueueTask(jobId, {
-          type: this.taskType,
-          parameters,
-        });
+        try {
+          await this.jobsClient.enqueueTask(jobId, {
+            type: this.taskType,
+            parameters,
+          });
+        } catch (err) {
+          // if (err instanceof NotFoundError) {
+          //   return null;
+          // } else {
+          //   this.logger.error(`[TaskHandler][dequeue] error=${JSON.stringify(err, Object.getOwnPropertyNames(err))}`);
+          //   throw err;
+          // }
+        }
+
       }
     }
   }
